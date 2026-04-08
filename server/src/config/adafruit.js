062a0802757b61867476e13b8546eb1ddf69d1c5
chicong14052005 <cong.ho1405@hcmut.edu.vn>
2026-04-08 10:54:47 +0700

    Update v5

--- server/src/config/adafruit.js
+++ server/src/config/adafruit.js
@@ -4,13 +4,13 @@ module.exports = {
   username: process.env.AIO_USERNAME || '',
   key: process.env.AIO_KEY || '',
   feeds: {
-    temperature: process.env.AIO_FEED_TEMPERATURE || 'yolofarm.temperature',
-    humidity: process.env.AIO_FEED_HUMIDITY || 'yolofarm.humidity',
-    soilMoisture: process.env.AIO_FEED_SOIL_MOISTURE || 'yolofarm.soil_moisture',
-    light: process.env.AIO_FEED_LIGHT || 'yolofarm.light',
-    pump1: process.env.AIO_FEED_PUMP1 || 'yolofarm.pump1',
-    pump2: process.env.AIO_FEED_PUMP2 || 'yolofarm.pump2',
-    led: process.env.AIO_FEED_LED || 'yolofarm.led_rgb'
+    temperature: process.env.AIO_FEED_TEMPERATURE || '',
+    humidity: process.env.AIO_FEED_HUMIDITY || '',
+    soilMoisture: process.env.AIO_FEED_SOIL_MOISTURE || '',
+    light: process.env.AIO_FEED_LIGHT || '',
+    pump1: process.env.AIO_FEED_PUMP1 || '',
+    pump2: process.env.AIO_FEED_PUMP2 || '',
+    led: process.env.AIO_FEED_LED || ''
   },
   restApiUrl: 'https://io.adafruit.com/api/v2'
 };
